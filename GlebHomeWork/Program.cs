b60751246bdbcf84faccf2d85aeda5955f1bc588
lolena1888 <lolena1888@hotmail.com>
2023-03-13 20:29:40 -0400

    Adding initial files

---
+++ GlebHomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using GlebsApplications;
+
+var price = 0.0f;
+while (true)
+{
+    var stan = new Stanford();
+    if (stan.VerifyPrice(72, price)) break;
+    price += 0.01f;
+}
+
+Console.WriteLine($"The price is ${price}. Total paid=${72 * price}");
+
---
+++ GlebHomeWork/Stenford.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace GlebsApplications
+{
+    internal class Stanford
+    {
+        public bool VerifyPrice(int num, float priceForOne)
+        {
+            const string compare = "67.9";
+            var price = (priceForOne * num).ToString(CultureInfo.InvariantCulture);
+            return (!price.EndsWith(compare) && !price.StartsWith(compare) && price.Contains(compare));
+        }
+    }
+}
